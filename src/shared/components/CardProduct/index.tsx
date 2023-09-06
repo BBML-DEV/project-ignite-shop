4951ec8196c737d78275c534a5aec1523a16d261
Paulo Vitor <pv.marques2010@hotmail.com>
2023-09-06 19:04:50 -0300

    feat: Finalizando o estilo do card do slider

--- src/shared/components/CardProduct/index.tsx
+++ src/shared/components/CardProduct/index.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
+import Link from 'next/link'
 
 interface CardProductProps {
-  productImage: StaticImageData
+  productImage: string
   productName: string
-  productPrice: number
+  productPrice: string
   customClass: string
   productId: string
+  reference: string
 }
 
 export const CardProduct = ({
@@ -15,25 +18,37 @@ export const CardProduct = ({
   customClass,
   productPrice,
   productId,
+  reference,
 }: CardProductProps) => {
   return (
-    <a
-      key={productId}
-      className={`card-product group relative flex cursor-pointer flex-col items-center justify-center rounded-lg bg-gradient-to-b from-gradientGreen to-gradientPurple p-1 shadow-md ${customClass}`}
-    >
-      <Image
-        src={productImage}
-        width={520}
-        height={480}
-        alt=""
-        className="object-cover"
-      />
-      <footer className="translateY-110 absolute bottom-1 left-1 right-1 flex transform items-center justify-between rounded-md bg-footerBackground p-8 opacity-0 transition-all duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
-        <strong className="text-lg text-gray300">{productName}</strong>
-        <span className="text-base font-bold text-green500">
-          {productPrice}
-        </span>
-      </footer>
-    </a>
+    <Link href={`/product/${reference}`} key={productId}>
+      <div
+        className={twMerge(
+          `card-product group relative flex cursor-pointer flex-col`,
+          `items-center justify-center rounded-lg bg-gradient-to-b from-gradientGreen to-gradientPurple`,
+          `p-1 shadow-md ${customClass}`,
+        )}
+      >
+        <Image
+          src={productImage}
+          width={520}
+          height={480}
+          alt=""
+          className="object-cover"
+        />
+        <footer
+          className={twMerge(
+            'translateY-110 absolute bottom-1 left-1 right-1 flex transform items-center ',
+            'justify-between rounded-md bg-footerBackground p-8 opacity-0',
+            'transition-all duration-200 ease-in-out group-hover:translate-y-0 group-hover:opacity-100',
+          )}
+        >
+          <strong className="text-lg text-gray300">{productName}</strong>
+          <span className="text-base font-bold text-green500">
+            {productPrice}
+          </span>
+        </footer>
+      </div>
+    </Link>
   )
 }
