51be5eb4ea49c644808bb9a736c508de2a64c841
Paulo Vitor <pv.marques2010@hotmail.com>
2023-09-11 12:04:41 -0300

    refix: corrigindo erro de tipagem

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,12 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+if (!stripeSecretKey) {
+  throw new Error('A chave secreta do Stripe não está definida.')
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2023-08-16',
   appInfo: {
     name: 'Ignite Shop',
